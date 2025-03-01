ed361b7284a6d578a036bfb7c3c7933fad106af3
Osalotioman <osalotioman@gmail.com>
2025-03-01 01:49:28 +0100

    Update guessing game to run in loop.

--- guess-the-number/main.c
+++ guess-the-number/main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int main(){
- int num = 5, guess; 
- printf("Enter guess: ");
- scanf("%d", &guess);
- if(num == guess){
-  printf("Guess correct.");
- }else{
-  printf("Guess failed.");
+ int secretNum = 5, guess; 
+ for(int i=0; i<3; ++i){
+    printf("Enter guess: ");
+    scanf("%d", &guess);
+    if(secretNum == guess){
+        printf("Guess correct.\n");
+    }else{
+        printf("Guess failed.\n");
+    }
  }
  return 0;
 }
